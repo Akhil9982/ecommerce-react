d6e348524863e7f9f6ffd797fc9ac8259c7374f3
Akhil Battula <46996299+Akhil9982@users.noreply.github.com>
2026-07-01 11:24:14 +0530

    Added CartContext for managing cart state; integrated cart functionality in ProductCard, Checkout, and ProductDetails components

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,14 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductById } from "../data/products";
 import { useEffect, useState } from "react";
+import { useCart } from "../context/CartContext";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const navigate = useNavigate();
+  const { addToCart, cartItems } = useCart();
+
   useEffect(() => {
     const foundProduct = getProductById(id);
     if (!foundProduct) {
@@ -13,12 +16,17 @@ const ProductDetails = () => {
       return;
     }
     setProduct(foundProduct);
-    console.log(foundProduct);
   }, [id]);
 
   if (!product) {
     return <h1>Loading...</h1>;
   }
+
+  const productInCart = cartItems.find((item) => item.id === product.id);
+
+  const productQuantityLabel = productInCart
+    ? `(${productInCart.quantity})`
+    : "";
   return (
     <div className="page">
       <div className="container">
@@ -30,7 +38,12 @@ const ProductDetails = () => {
             <h1 className="product-detail-name">{product.name}</h1>
             <p className="product-detail-price">${product.price}</p>
             <p className="product-detail-description">{product.description}</p>
-            <button className="btn btn-primary">Add to Cart</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => addToCart(product.id)}
+            >
+              Add to Cart {productQuantityLabel}
+            </button>
           </div>
         </div>
       </div>
